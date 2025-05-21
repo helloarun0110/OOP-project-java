24c184b89924eae6dbd02cbafbb73c2fe5d72adc
Arun <helloarun0110@gmail.com>
2025-05-21 22:54:04 +0600

    complete the GameFrame module => control all game window operations

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,3 +1,27 @@
-public class GameFrame {
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    private final int tileSize = 32;
+    private final int rows = 21;
+    private final int cols = 19;
+
+    public GameFrame(){
+        setTitle("Pac-Man");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        GamePanel panel = new GamePanel(rows, cols, tileSize);
+        add(panel);
+        pack();
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        panel.requestFocusInWindow(); //Makes the GamePanel receive keyboard input focus
+        
+    }
+
+    public void start(){}
     
 }
