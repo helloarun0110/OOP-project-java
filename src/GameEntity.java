02e6309cbcb62821062fb05a493c68e8a4f0af59
Arun <helloarun0110@gmail.com>
2025-05-21 23:04:44 +0600

    complete the GameEntity module => work as a base class for all game character adn contail collision detection logic make it common for all

--- src/GameEntity.java
+++ src/GameEntity.java
@@ -1,3 +1,34 @@
+import java.awt.*;
+import javax.swing.ImageIcon;
+
 public class GameEntity {
+    protected int x, y, size, oldX, oldY;
+    protected Image image;
+
+    public GameEntity(int x, int y, int size){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.oldX = x;
+        this.oldY = y;
+
+    }
+
+    public void setImage(String path){
+
+        ImageIcon icon = new ImageIcon(path);
+        this.image = icon.getImage().getScaledInstance(size, size, Image.SCALE_SMOOTH);
+    }
+
+    public void draw(Graphics g){
+        if (image != null){
+            g.drawImage(image, x, y, null);
+        }
+    }
+
+    public boolean collidesWith(GameEntity other){
+        return x < other.x + other. size && x + size >other.x && y < other.y + other.size && y + size > other.y;
+    }
+
     
 }
